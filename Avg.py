a415f25e3d7682c5c25960cf36170e7d155f7306
priyadarshanisahoo20-cloud <priyadarshanisahoo20@gmail.com>
2026-06-02 11:25:26 +0530

    Add files via upload

---
+++ Avg.py
@@ -0,0 +1,11 @@
+# Input 4 numbers
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+d = float(input("Enter fourth number: "))
+
+# Calculate average
+avg = (a + b + c + d) / 4
+
+# Display result
+print("Average =", avg)
---
+++ student_mark.py
@@ -0,0 +1,14 @@
+# Input marks of 5 subjects
+s1 = float(input("Enter marks of Subject 1: "))
+s2 = float(input("Enter marks of Subject 2: "))
+s3 = float(input("Enter marks of Subject 3: "))
+s4 = float(input("Enter marks of Subject 4: "))
+s5 = float(input("Enter marks of Subject 5: "))
+
+# Calculate total and average
+total = s1 + s2 + s3 + s4 + s5
+average = total / 5
+
+# Display result
+print("Total Marks =", total)
+print("Average Marks =", average)
