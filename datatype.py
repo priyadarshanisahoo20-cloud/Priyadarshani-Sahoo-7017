7e0708a0d1ea1fb9565cca4cc7be1582cac14e3b
priyadarshanisahoo20-cloud <priyadarshanisahoo20@gmail.com>
2026-06-02 08:55:19 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,22 @@
+a = 10
+
+b = 3.14
+
+c = "Hello"
+
+d = True
+
+e = 'S'
+
+
+print("Integer:", a)
+print("Float:", b)
+print("String:", c)
+print("Boolean:", d)
+print("Character:",e)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
